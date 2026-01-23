ee9f1d8fee276d8ce30c6570562f05ffb83da02c
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-23 20:06:18 +0800

    feat: added basic necessary code for fastapi. (issue fixed #2)

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from notif-me-senpai!")
+from fastapi import FastAPI, status
+
+
+app = FastAPI()
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+def read_root():
+    return {"Hello": "World"}
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+
+    print("RUNNING UVICORN")
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
