bb005f06f1961ad521dd4560fe18cd0913d41610
Nana Mein <alfeoephraimquintay@gmail.com>
2026-02-04 21:24:17 +0800

    Feat: Added async function wrapper that handles chat completion of Groq as inference api call with MemoryAsyncDB that handle message persistence.

--- backend/chat_completions/chatbot.py
+++ backend/chat_completions/chatbot.py
@@ -1,10 +1,35 @@
-from langchain_core.prompts import ChatPromptTemplate
+from typing import Any, Union, Tuple
+from backend.databases.memory import MemoryAsyncDB
+from langchain_groq.chat_models import ChatGroq
+from backend.core.env_file import settings
+from langchain_core.messages import BaseMessage
 
-
-prompt = ChatPromptTemplate(
-    [
-        ("system","You are a helpful assistant"),
-        ("user","{question}"),
-    ]
+chat = ChatGroq(
+    api_key=settings.GROQ_API_KEY.get_secret_value(),
+    temperature=.7,
+    model="llama-3.1-8b-instant",
+    max_tokens=8000
 )
-print(prompt.messages)
+Response_Type = Union[str, list[Union[str,dict]]]
+List_Types = list[BaseMessage]
+Tuple_type = Tuple[Response_Type, List_Types]
+
+async def ask_query(user_id: Union[str, Any], query: str) -> Tuple_type:
+    if not isinstance(user_id, str):
+        user_id = str(user_id)
+
+    mem = MemoryAsyncDB(user_id=user_id)
+
+    await mem.add_human_message(content=query)
+
+    messages = await mem.get_messages_with_system()
+
+    resp = await chat.ainvoke(messages)
+
+    response = resp.content
+
+    await mem.add_ai_message(content=response)
+
+    list_message = await mem.get_messages_with_system()
+
+    return response, list_message
