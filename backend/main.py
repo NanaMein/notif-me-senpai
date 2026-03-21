236302ce32d3b1fac48efb41b8eb5a887488d9d4
Nana Mein <alfeoephraimquintay@gmail.com>
2026-03-22 00:51:07 +0800

    Feature: Added Middleware CORS and trusted Hosts

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI, status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from routers.v1 import router
 
 
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=["*"],
+)
 app.include_router(router)
+
 
 
 if __name__ == "__main__":
